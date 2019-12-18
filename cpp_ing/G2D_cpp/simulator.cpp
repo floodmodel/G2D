4be8e7d10f0f61c2dcc9948a61e278fc95e3b340
Yun Seok Choi <yschoi51@kict.re.kr>
2019-12-18 17:31:54 +0900

    cpp ~ing

--- cpp_ing/G2D_cpp/simulator.cpp
+++ cpp_ing/G2D_cpp/simulator.cpp
@@ -95,50 +95,42 @@ int simulationControlUsingCPUnGPU()
 		}
 		initilizeThisStep(psi.dt_sec, ps.tnow_sec, dtbc_sec, rfEnded);
 
-		//	// gpu 옵션이 true 인 경우에도 지정셀(모로코에서는 40,000 개 가 적당) 셀 이상을 모의할 때만 gpu를 사용한다.
-		//	// 모의 대상 셀 개수가 작을 때는 cpu 가 더 빠르다.
-		//	if (cGenEnv.usingGPU == 1 && cThisProcess.effCellCount > cGenEnv.EffCellThresholdForGPU)
-		//	{
-		//		//sw.Start();
-		//		if (onCPU == 1)
-		//		{
-		//			cGenEnv.writelog("Calculation was converted into GPU. ", true);
-		//			onCPU = -1;
-		//		}
-		//		mSolver.RunSolverUsingGPU(cvs, cvsadd, ref gv, bcCellinfo);
-		//		//sw.Stop();
-		//		//File.AppendAllText(logFPN, cGenEnv.tnow_min.ToString("F2") + "min. RunSolverUsingGPU, elaplsed time [ms] : " +
-		//		//                       sw.Elapsed.TotalMilliseconds.ToString()  + "\r\n");
-		//		//sw.Reset();
-		//	}
-		//	else
-		//	{
-		//		//sw.Start();
-		//		if (onCPU == -1)
-		//		{
-		//			cGenEnv.writelog("Calculation was converted into CPU. ", true);
-		//			onCPU = 1;
-		//		}
-		//		mSolver.RunSolverUsing1DArray(cvs, cvsadd, gv[0].dt_sec, gv, bcCellinfo);
-		//		//sw.Stop();
-		//		//File.AppendAllText(logFPN, cGenEnv.tnow_min.ToString("F2") + "min. RunSolverUsingCPU, elaplsed time [ms] : " +
-		//		//    sw.Elapsed.TotalMilliseconds.ToString() + " simCell :  " + cGenEnv.effCellCount.ToString() + "\r\n");
-		//		//sw.Reset();
-		//	}
-		//	// 여기까지..
+		// gpu 옵션이 true 인 경우에도 지정셀(모로코에서는 40,000 개 가 적당) 셀 이상을 모의할 때만 gpu를 사용한다.
+		// 모의 대상 셀 개수가 작을 때는 cpu 가 더 빠르다.
+		if (prj.usingGPU == 1 && psi.effCellCount > prj.effCellThresholdForGPU)
+		{
+			if (onCPU == 1) {
+				writeLog(fpn_log, "Calculation was converted into GPU. ", 1, 1);
+				onCPU = -1;
+			}
+			mSolver.RunSolverUsingGPU(cvs, cvsadd, ref gv, bcCellinfo);
+			//File.AppendAllText(logFPN, cGenEnv.tnow_min.ToString("F2") + "min. RunSolverUsingGPU, elaplsed time [ms] : " +
+			//                       sw.Elapsed.TotalMilliseconds.ToString()  + "\r\n");
+		}
+		else
+		{
+			if (onCPU == -1) {
+				writeLog(fpn_log, "Calculation was converted into CPU. ", 1, 1);
+				onCPU = 1;
+			}
+			mSolver.RunSolverUsing1DArray(cvs, cvsadd, gv[0].dt_sec, gv, bcCellinfo);
+			//File.AppendAllText(logFPN, cGenEnv.tnow_min.ToString("F2") + "min. RunSolverUsingCPU, elaplsed time [ms] : " +
+			//    sw.Elapsed.TotalMilliseconds.ToString() + " simCell :  " + cGenEnv.effCellCount.ToString() + "\r\n");
+		}
+		// 여기까지..
 
-		//	cGenEnv.iGS = gv[0].iGS;
-		//	cGenEnv.iNR = gv[0].iNR;
-		//	if (cGenEnv.tnow_sec >= cGenEnv.tsec_targetToprint)
-		//	{
-		//		checkEffetiveCellNumberAndSetAllFlase(cvs);// 매번 업데이트 하지 않고, 출력할때 마다 이 정보 업데이트 한다.
-		//		prj.output.makeOutputFilesUsing1DArray(cvs, cvsadd, cGenEnv.tnow_sec, cGenEnv.dt_printout_min, prj.domain.nodata_value);
-		//		SimulationStep(cGenEnv.tnow_min);
-		//		if (onCPU == 1) { Gpu.FreeAllImplicitMemory(true); }
-		//		if (UpdateSimulaltionParameters(prj, Path.Combine(prj.prjFilePath, prj.prjFileName)) == false) { return false; } //한번 출력할때 마다 모의변수 업데이트
-		//		cGenEnv.tsec_targetToprint = cGenEnv.tsec_targetToprint + cGenEnv.dt_printout_sec;
-		//		cGenEnv.thisPrintStepStartTime = DateTime.Now;
-		//	}
+	//	cGenEnv.iGS = gv[0].iGS;
+	//	cGenEnv.iNR = gv[0].iNR;
+	//	if (cGenEnv.tnow_sec >= cGenEnv.tsec_targetToprint)
+	//	{
+	//		checkEffetiveCellNumberAndSetAllFlase(cvs);// 매번 업데이트 하지 않고, 출력할때 마다 이 정보 업데이트 한다.
+	//		prj.output.makeOutputFilesUsing1DArray(cvs, cvsadd, cGenEnv.tnow_sec, cGenEnv.dt_printout_min, prj.domain.nodata_value);
+	//		SimulationStep(cGenEnv.tnow_min);
+	//		if (onCPU == 1) { Gpu.FreeAllImplicitMemory(true); }
+	//		if (UpdateSimulaltionParameters(prj, Path.Combine(prj.prjFilePath, prj.prjFileName)) == false) { return false; } //한번 출력할때 마다 모의변수 업데이트
+	//		cGenEnv.tsec_targetToprint = cGenEnv.tsec_targetToprint + cGenEnv.dt_printout_sec;
+	//		cGenEnv.thisPrintStepStartTime = DateTime.Now;
+	//	}
 		tnow_min_bak = ps.tnow_min;
 		ps.tnow_sec = ps.tnow_sec + psi.dt_sec;
 		//	if (cGenEnv.isfixeddt == -1)
